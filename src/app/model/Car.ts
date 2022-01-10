acdd86bd6f6d1d435e2f760b62593c1274ef9549
BrenoRev <silvabreno462@gmail.com>
2022-01-10 15:14:07 -0300

    Firsts configuration

---
+++ src/app/model/Car.ts
@@ -0,0 +1,18 @@
+import { Brand } from './Brand';
+import { Model } from './Model';
+export interface Car{
+    id: number;
+    model : Model;
+    brand: Brand;
+    name: string;
+    ageCar: number;
+    km: number;
+    price: number;
+    ipva: boolean;
+    reserved: boolean;
+    photo: string;
+    carType: string;
+    carTransmission: string;
+    carColor: string;
+    carFuel: string;
+}
